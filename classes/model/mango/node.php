f798fbad0d929f95e11cce2404ed27615f38f89c
Bryan Galli <bryangalli@gmail.com>
2012-02-15 16:11:21 -0700

    the models

---
+++ classes/model/mango/node.php
@@ -0,0 +1,44 @@
+<?php
+class Model_Mango_Node extends Mango {
+
+	protected $_fields = array(
+		'question'	=> array(
+			'type'		=>'string',
+			'required' 	=> true,
+			'not_empty'	=> true,
+		),
+		'answers'		=> array(
+			'type'		=>'array',
+		),
+		'created'		=> array(
+			'type'		=> 'string',
+			'required'	=> true,
+			'rules' => 
+			    array( 
+			    	array( array(':model', 'check_timestamp')), 
+			    )
+		),
+		'action' => array(
+			'type'		=> 'string',
+		),
+		'creator'	=> array(
+			'type'		=> 'string',
+		),
+	);
+
+	protected $_db = 'localhost'; //don't use default db config
+	
+	public function check_timestamp($val) 
+    {
+        if (!empty($val))
+        {
+            if(is_numeric($val))
+            {
+                if(strlen($val) > 6)
+                {
+                    return true;    
+                } else { return false; }
+            } else { return false; }        
+        } else { return false; }
+    }
+}
---
+++ classes/model/mango/scenario.php
@@ -0,0 +1,51 @@
+<?php
+class Model_Mango_Scenario extends Mango {
+
+	protected $_fields = array(
+		'description'	=> array(
+			'type'		=>'string',
+			'required' 	=> true,
+			'not_empty'	=> true,
+		),
+		'title'	=> array(
+			'type'		=>'string',
+			'required' 	=> true,
+			'not_empty'	=> true,
+		),
+		'starting_node'		=> array(
+			'type'		=>'string',
+			'required' 	=> true,
+			'not_empty'	=> true,
+		),
+		'created'		=> array(
+			'type'		=> 'string',
+			'required'	=> true,
+			'not_empty'	=> true,
+			'rules' => 
+			    array( 
+			    	array( array(':model', 'check_timestamp')), 
+			    )
+		),
+		'creator'	=> array(
+			'type'		=> 'string',
+			'required'	=> true,
+			'not_empty'	=> true,
+		),
+	);
+
+	protected $_db = 'localhost'; //don't use default db config
+	
+	public function check_timestamp($val) 
+    {
+        if (!empty($val))
+        {
+            if(is_numeric($val))
+            {
+                if(strlen($val) > 6)
+                {
+                    return true;    
+                } else { return false; }
+            } else { return false; }        
+        } else { return false; }
+    }
+}
